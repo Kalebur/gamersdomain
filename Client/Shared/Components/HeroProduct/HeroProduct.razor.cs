86b9bc5c0911de46d49549acdb017c04d00cec0c
James Smith <kaleburdev@gmail.com>
2023-10-30 21:57:14 -0400

    Add hero product component and some product images

---
+++ Client/Shared/Components/HeroProduct/HeroProduct.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace gamersdomain.Client.Shared.Components.HeroProduct
+{
+    public partial class HeroProduct
+    {
+        [Parameter]
+        public string? ImageUrl { get; set; } = string.Empty;
+
+        [Parameter]
+        public string? ProductName { get; set; } = string.Empty;
+        [Parameter]
+        public string? Description { get; set; } = string.Empty;
+        [Parameter]
+        public string? Price { get; set; } = string.Empty;
+    }
+}
