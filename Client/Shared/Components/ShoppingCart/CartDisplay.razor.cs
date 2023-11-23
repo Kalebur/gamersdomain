098ff4ba3c6c01ee4b17b975dba25c082703dc73
James Smith <kaleburdev@gmail.com>
2023-11-22 20:54:00 -0500

    Add local storage shopping cart

--- Client/Shared/Components/ShoppingCart/CartDisplay.razor.cs
+++ Client/Shared/Components/ShoppingCart/CartDisplay.razor.cs
@@ -1,8 +1,25 @@
-﻿namespace gamersdomain.Client.Shared.Components.ShoppingCart
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace gamersdomain.Client.Shared.Components.ShoppingCart
 {
     public partial class CartDisplay
     {
         public int TotalItems { get; set; }
         public decimal TotalPrice { get; set; }
+        public List<CartItem> Items { get; set; } = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            bool cartExists = await localStorage.ContainKeyAsync("gd-cart");
+            if (!cartExists)
+            {
+                await localStorage.SetItemAsync<List<CartItem>>("gd-cart", Items);
+            }
+            else
+            {
+                List<CartItem> itemsFromCart = await localStorage.GetItemAsync<List<CartItem>>("gd-cart");
+                Console.WriteLine($"Item Count: {itemsFromCart.Count}");
+            }
+        }
     }
 }
